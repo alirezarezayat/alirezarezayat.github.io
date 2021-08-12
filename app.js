1185809bf1eb8d62654232570bd1a02315eaffd2
alirezarezayat <alireza_rezayat1@yahoo.com>
2021-08-12 09:28:55 +0430

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ fetch("https://api.github.com/users/alirezarezayat")
       .then((data) => {
         console.log(data);
         console.log(data.length());
+        console.log(data[0]);
+        console.log(data[0].id);
       });
     document.getElementById("repositories").innerHTML = data.repos_url;
   });
