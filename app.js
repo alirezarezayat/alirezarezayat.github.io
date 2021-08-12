e4ba430b5ebaece82669b5e966905c187d96e10d
alirezarezayat <alireza_rezayat1@yahoo.com>
2021-08-12 10:54:31 +0430

    Update app.js

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ fetch("https://api.github.com/users/alirezarezayat")
       .then((data) => {
         for (let variable in data) {
           console.log(data[variable]);
-          document.getElementById(
-            "repositories"
-          ).innerHTML += `${data[variable].name}`;
+          document.getElementById("repositories").innerHTML = (
+            <li id="repos-${variable}">
+              <a id="repos-${variable}-url">`${data[variable].name}`</a>
+            </li>
+          );
         }
       });
   });
