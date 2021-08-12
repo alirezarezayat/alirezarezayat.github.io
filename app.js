fd15b027ad1724060977a618c97ec92351007dbc
alirezarezayat <alireza_rezayat1@yahoo.com>
2021-08-12 09:17:38 +0430

    access for repositories

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ fetch("https://api.github.com/users/alirezarezayat")
   .then((data) => {
     document.getElementById("avatar").src = `${data.avatar_url}`;
     document.getElementById("name").innerHTML = `${data.name}`;
-    fetch(data.repos_url).then((result) => {
-      console.log(result);
-    });
+    fetch(data.repos_url)
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data);
+      });
     document.getElementById("repositories").innerHTML = data.repos_url;
   });
