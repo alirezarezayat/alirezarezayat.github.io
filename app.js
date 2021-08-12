e35df619803263008235770fd43abc453828edad
alirezarezayat <alireza_rezayat1@yahoo.com>
2021-08-12 10:39:29 +0430

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 fetch("https://api.github.com/users/alirezarezayat")
-    .then((result) => result.json())
-    .then((data) => {
-        document.getElementById("avatar").src = `${data.avatar_url}`;
-        document.getElementById("fav-icon").href = `${data.avatar_url}`;
-        document.getElementById("page-title").innerHTML = `${data.name}`;
-        document.getElementById("name").innerHTML = `${data.name}`;
-        document.getElementById("bio").innerHTML = `${data.bio}`;
-        document.getElementById("my-github-url").href = `${data.html_url}`;
-        fetch(data.repos_url)
-            .then((result) => result.json())
-            .then((data) => {
-                for (let variable in data) {
-                    console.log(data[variable]);
-                    document.getElementById("repositories").innerHTML = <li id=`repos-${variable}` > variable</li >;
-                }
-            });
-    });
+  .then((result) => result.json())
+  .then((data) => {
+    document.getElementById("avatar").src = `${data.avatar_url}`;
+    document.getElementById("fav-icon").href = `${data.avatar_url}`;
+    document.getElementById("page-title").innerHTML = `${data.name}`;
+    document.getElementById("name").innerHTML = `${data.name}`;
+    document.getElementById("bio").innerHTML = `${data.bio}`;
+    document.getElementById("my-github-url").href = `${data.html_url}`;
+    fetch(data.repos_url)
+      .then((result) => result.json())
+      .then((data) => {
+        for (let variable in data) {
+          console.log(data[variable]);
+          document.getElementById(
+            "repositories"
+          ).innerHTML += `${data[variable].name}`;
+        }
+      });
+  });
