eea257d8344fa53f3d83dd8febd633f009f4dae2
Alireza Rezayat <alireza_rezayat1@yahoo.com>
2022-11-20 15:38:59 +0330

    upload my image . comment js useless codes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 fetch("https://api.github.com/users/alirezarezayat")
   .then((result) => result.json())
   .then((data) => {
-    document.getElementById("avatar").src = `${data.avatar_url}`;
-    document.getElementById("fav-icon").href = `${data.avatar_url}`;
-    document.getElementById("page-title").innerHTML = `${data.name}`;
-    document.getElementById("name").innerHTML = `${data.name}`;
+    // document.getElementById("avatar").src = `${data.avatar_url}`;
+    // document.getElementById("fav-icon").href = `${data.avatar_url}`;
+    // document.getElementById("page-title").innerHTML = `${data.name}`;
+    // document.getElementById("name").innerHTML = `${data.name}`;
     document.getElementById("bio").innerHTML = `${data.bio}`;
     document.getElementById("my-github-url").href = `${data.html_url}`;
     fetch(data.repos_url)
