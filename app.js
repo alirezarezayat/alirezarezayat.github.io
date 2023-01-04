672d789cb9d12ea3faee02b5245c2eeb1d465726
Alireza Rezayat <alireza_rezayat1@yahoo.com>
2023-01-04 19:31:23 +0330

    use more bootstrap
    try to change view in mobile and desktop

--- app.js
+++ app.js
@@ -1,34 +1,68 @@
 fetch("https://api.github.com/users/alirezarezayat")
   .then((result) => result.json())
   .then((data) => {
-    // document.getElementById("avatar").src = `${data.avatar_url}`;
-    // document.getElementById("fav-icon").href = `${data.avatar_url}`;
-    // document.getElementById("page-title").innerHTML = `${data.name}`;
-    // document.getElementById("name").innerHTML = `${data.name}`;
-    document.getElementById("bio").innerHTML = `${data.bio}`;
-    document.getElementById("my-github-url").href = `${data.html_url}`;
     fetch(data.repos_url)
       .then((result) => result.json())
       .then((data) => {
         for (let variable in data) {
           // console.log(data[variable]);
           // console.log(data[variable].name);
-          document.getElementById(
-            "repositories"
-          ).innerHTML += `<div id="repos-div">
-            <li id="repos-${variable}">
-              <a id="${data[variable].name}" href="${data[variable].html_url}">
+          document.getElementById("repositories").innerHTML += `
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-header">
                   ${data[variable].name}
-              </a>
-            </li>
-            <p>
-              page of this repository is : 
-              <a href="https://alirezarezayat.github.io/${data[variable].name}" target="_blank">
-                ${data[variable].name}
-              </a>
-            </p>
-            <p>${data[variable].description}</p></div>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Repository :
+                    <a id="${data[variable].name}" href="${data[variable].html_url}">${data[variable].name}</a>
+                  </h5>
+                  <p class="card-text">${data[variable].description}</p>
+                  <a  href="https://alirezarezayat.github.io/${data[variable].name}" target="_blank" class="btn btn-primary">${data[variable].name} Page</a>
+                </div>
+              </div>
+            </div>
           `;
         }
+        document.getElementById('spinner').remove();
       });
   });
+
+const avatar = document.getElementById("avatar");
+if (window.screen.width < 576) avatar.style.width = "33%";
+
+window.addEventListener("scroll", function () {
+  // console.log(this.document.activeElement);
+  // avatar.style.width = 33 + "%";
+  if (this.window.screen.width < 576)
+    if (
+      // document.body.scrollTop > 50 ||
+      // document.documentElement.scrollTop > 50 ||
+      this.document.documentElement.scrollTop < 250
+    ) {
+      avatar.style.width = "33%";
+    } else {
+      avatar.style.width = "max";
+    }
+  console.log(avatar.style.width);
+});
+
+function onClickAvatar() {
+  if (avatar.style.width != "40%") {
+    console.log(avatar.style.width);
+    if (avatar.style.width == "100%") {
+      avatar.style.width = "33%";
+    } else avatar.style.width = "40%";
+  } else {
+    avatar.style.width = "33%";
+  }
+  // avatar.removeAttribute("style");
+}
+
+function onLeaveAvatar() {
+  avatar.style.width = "20%";
+}
+
+function onMouseOverAvatar() {
+  avatar.style.width = "30%";
+}
